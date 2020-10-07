1fb9823d39e29e717000a8287365fabc13846017
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2020-10-08 00:06:09 +0900

    reducer 정의 및 미들웨어 사용

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';;
+import { applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-import 'antd/dist/antd.css';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider
+  store = {createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+
+    <App/>
+    </Provider>
+  ,
   document.getElementById('root')
 );
 
