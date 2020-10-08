0c1e25e4e084ae15fb70c45acd1759354e57bbef
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2020-10-08 13:48:34 +0900

    회원가입 추가

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
 
 export function loginUser(dataTosubmit) {
@@ -9,7 +10,18 @@ export function loginUser(dataTosubmit) {
     .then(response => response.data )
 
     return {
-        type:"LOGIN_USER" ,
+        type:LOGIN_USER ,
+        payload : request
+    }
+}
+
+export function RegisterUser(dataTosubmit) {
+
+    const request = axios.post('/api/users/Register',dataTosubmit)
+    .then(response => response.data )
+
+    return {
+        type: REGISTER_USER ,
         payload : request
     }
 }
